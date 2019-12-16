e7e425752846dd5e1f597e87bcf687502efe1f9e
chehsingchu <49218091+chehsingchu@users.noreply.github.com>
2019-12-16 13:21:00 +0800

    Add files via upload

--- mongodb.py
+++ mongodb.py
@@ -18,7 +18,7 @@ def constructor():
 #----------------------------儲存使用者的股票--------------------------
 def write_user_stock_fountion(stock, bs, price):  
     db=constructor()
-    collect = db['chuc']
+    collect = db['mystock']
     collect.insert({"stock": stock,
                     "data": 'care_stock',
                     "bs": bs,
@@ -29,13 +29,13 @@ def write_user_stock_fountion(stock, bs, price):
 #----------------------------殺掉使用者的股票--------------------------
 def delete_user_stock_fountion(stock):  
     db=constructor()
-    collect = db['chuc']
+    collect = db['mystock']
     collect.remove({"stock": stock})
     
 #----------------------------秀出使用者的股票--------------------------
 def show_user_stock_fountion():  
     db=constructor()
-    collect = db['chuc']
+    collect = db['mystock']
     cel=list(collect.find({"data": 'care_stock'}))
 
     return cel
