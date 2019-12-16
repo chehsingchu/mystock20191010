8710173da96766ba04b42b31f4370e9292e8d372
chehsingchu <49218091+chehsingchu@users.noreply.github.com>
2019-12-16 12:49:26 +0800

    Add files via upload

---
+++ mongodb.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from pymongo import MongoClient
+import urllib.parse
+import datetime
+###############################################################################
+#                       股票機器人 Python基礎教學 【pymongo教學】                      #
+###############################################################################
+
+# Authentication Database認證資料庫
+Authdb='mydb'
+
+##### 資料庫連接 #####
+def constructor():
+    client = MongoClient('client = pymongo.MongoClient("mongodb://chuc:chuc0315@@cluster0-shard-00-00-6dvcu.mongodb.net:27017,cluster0-shard-00-01-6dvcu.mongodb.net:27017,cluster0-shard-00-02-6dvcu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")')
+    db = client.test
+    return db
+   
+#----------------------------儲存使用者的股票--------------------------
+def write_user_stock_fountion(stock, bs, price):  
+    db=constructor()
+    collect = db['chuc']
+    collect.insert({"stock": stock,
+                    "data": 'care_stock',
+                    "bs": bs,
+                    "price": float(price),
+                    "date_info": datetime.datetime.utcnow()
+                    })
+    
+#----------------------------殺掉使用者的股票--------------------------
+def delete_user_stock_fountion(stock):  
+    db=constructor()
+    collect = db['chuc']
+    collect.remove({"stock": stock})
+    
+#----------------------------秀出使用者的股票--------------------------
+def show_user_stock_fountion():  
+    db=constructor()
+    collect = db['chuc']
+    cel=list(collect.find({"data": 'care_stock'}))
+
+    return cel
+
+
+
